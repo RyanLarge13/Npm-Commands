4e33b54678ccd477f750f5bb4653da93f7efa0c2
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-10-05 15:07:07 -0700

    Finishing up passport authentication.

--- auth/passport.js
+++ auth/passport.js
@@ -1,28 +1,28 @@
-import bcrypt from 'bcryptjs';
-import localStrategy from 'passport-local';
-import { User } from '../models/userModel.js';
+import bcrypt from "bcryptjs";
+import localStrategy from "passport-local";
+import { User } from "../models/userModel.js";
 localStrategy.Strategy;
 
-export const initialize =  (passport, getUserById) => {
-    const authenticateUser = (email, password, done) => {
-        User.findOne({ Email: email }).then(async (user) => {
-            if (!user) return done(null, false, { message: 'No user with that email.'})
-            try {
-                if (await bcrypt.compare(password, user.Password)) {
-                    return done(null, user);
-                } else {
-                    return done(null, false, { message: 'Password is incorrect'});
-                }
-            } catch (err) {
-                return done(err);
-            }
-        });
-    };
-
-    passport.use(new localStrategy({ usernameField: 'email' }, authenticateUser));
-    passport.serializeUser((user, done) => done(null, user._id));
-    passport.deserializeUser((id, done) => {
-        return done(null, getUserById(id))
+export const initialize = async (passport, getUserById) => {
+  const authenticateUser = async (email, password, done) => {
+    await User.findOne({ Email: email }).then(async (user) => {
+      if (!user)
+        return done(null, false, { message: "No user with that email." });
+      try {
+        if (await bcrypt.compare(password, user.Password)) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Password is incorrect" });
+        }
+      } catch (err) {
+        return done(err);
+      }
     });
+  };
 
+  passport.use(new localStrategy({ usernameField: "email" }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user._id));
+  passport.deserializeUser(async (id, done) => {
+    return done(null, await getUserById(id));
+  });
 };
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import { User } from './models/userModel.js';
+import { User } from "./models/userModel.js";
 import { connectDB } from "./config/db.js";
 import { router } from "./routes/commandRoutes.js";
 import { registerRouter } from "./routes/registerRoute.js";
 // import { loginRouter } from "./routes/loginRoute.js";
-import { initialize } from './auth/passport.js';
-import passport from 'passport';
-import flash from 'express-flash';
-import session from 'express-session';
-initialize(passport, id => User.find({ _id: id }).then((user) => {
-    return user
-}));
+import { initialize } from "./auth/passport.js";
+import passport from "passport";
+import flash from "express-flash";
+import session from "express-session";
+initialize(passport, (id) =>
+  User.find({ _id: id }).then((user) => {
+    return user;
+  })
+);
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
@@ -23,11 +25,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(flash());
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.set("view engine", "ejs");
@@ -35,27 +39,33 @@ app.use(express.static("views"));
 // app.use("/", router, registerRouter, loginRouter);
 app.use("/", router, registerRouter);
 
-app.get('/login', (req, res) => {
-    res.render('html/login');
+app.get("/login", (req, res) => {
+  res.render("html/login");
 });
 
-app.get('/dashboard', (req, res) => {
-    console.log(req.user)
-    User.findOne({ User: req.user }).then((user) => {
-        if (!user) console.log('ERROr!!!!!!!!!')
-        else {
-            res.render('html/dashboard', {
-                name: user.Username 
-            });
-        }
-    });
+app.get("/dashboard", (req, res) => {
+  /* console.log(req.user[0].Username);
+  res.render("html/dashboard", {
+    name: req.user.Username,
+  }); */
+  User.findOne({ Username: req.user[0].Username }).then((user) => {
+    if (!user) console.log("ERROR!!!!!!!!!");
+    else {
+      res.render("html/dashboard", {
+        name: user.Username,
+      });
+    }
+  });
 });
 
-app.post('/login', passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/login',
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
     failureFlash: true,
-}))
+  })
+);
 
 connectDB();
 
